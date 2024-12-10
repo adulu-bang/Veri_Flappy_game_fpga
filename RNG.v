3e550de1967e2cf4e152ceb10677c2796de7388a
adulu-bang <161625798+adulu-bang@users.noreply.github.com>
2024-12-10 17:24:32 +0530

    Update RNG.v

--- RNG.v
+++ RNG.v
@@ -21,18 +21,17 @@
 module RNG( input clk, output reg[7:0] out
     );
 	 
-	// input clk;
-	//output reg [7:0] out;
+	
 	 
 	reg [20:0] rand;
-	// initial rand = ~(20'b0);
-	initial rand= ~(21'b0);
+	initial rand = ~(20'b0);
+	//initial rand= ~(21'b0);
 			
         reg [20:0] rand_next;
 	wire feedback;
 	 
-	// assign feedback = rand[20] ^ rand[17];
-	assign feedback=rand[20]^rand[17];
+        assign feedback = rand[20] ^ rand[17];
+	//assign feedback=rand[20]^rand[17];
 	
 	 always @ (posedge clk)
 	 begin
